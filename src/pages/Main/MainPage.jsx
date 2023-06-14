0c69e474caf78cdb6d175f25ca58d98a9f5f73b8
magoador <malikmagomedov95@mail.ru>
2023-06-15 00:17:36 +0300

    Сверстал 6 блок

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import Block1 from '../../components/Block1/Block1';
+import React from "react";
+import Block1 from "../../components/Block1/Block1";
 import Block2 from "../../components/Block2/Block2";
-import styles from './MainPage.module.scss'
-import Block5 from '../../components/Block5/Block5';
-import Block34 from '../../components/Block34/Block34';
-const MainPage = () => {
-    return (
-        <div className={styles.MainPage}>
-            <Block1 />
-            <Block2 />
-            <Block34 />
-            <Block5 />
-        </div>
-    );
+import styles from "./MainPage.module.scss";
+import Block5 from "../../components/Block5/Block5";
+import Block34 from "../../components/Block34/Block34";
+import Block6 from "../../components/Block6/Block6";
 
+const MainPage = () => {
+  return (
+    <div className={styles.MainPage}>
+      <Block1 />
+      <Block2 />
+      <Block34 />
+      <Block5 />
+      <Block6 />
+    </div>
+  );
 };
 
 export default MainPage;
