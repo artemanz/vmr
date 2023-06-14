4f63356fd863a4f3ab7f8f1d844410fc94ed06bf
saraliev <adamfuturama@mail.ru>
2023-06-14 14:09:50 +0300

    добавил 5 блок и сделал адаптив

---
+++ src/components/Block1/Block1.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Block1.module.scss";
+import Img from "../../assets/img/Eye/eye.png";
+import ImgW from "../../assets/img/Eye/eyeWhite.png";
+
+const Block1 = () => {
+  return (
+    <div className={styles.Block1}>
+      <div className={styles.Block1_Info}>
+        <span>
+          <strong>ВИДЕОМОНИТОРИНГ</strong>– многогранный исследовательский
+          инструмент, позволяющий корректировать поведенческие алгоритмы
+          сотрудников и с их помощью влиять на реакцию и решение покупателей.
+          Что в свою очередь открывает возможность повысить уровень продаж и
+          удовлетворённость клиентов.
+        </span>
+        <div className={styles.Block1_Info_Img}>
+          <img src={ImgW} alt="eye" />
+        </div>
+
+        <span>
+          <strong>ВИДЕОАНАЛИТИКА</strong>– наиболее оптимальный инструмент с
+          точки зрения бюджета и количества обрабатываемых данных. С его помощью
+          вы всегда будете в курсе актуальной информации на местах, а проработка
+          алгоритмов персонального обслуживания успешно повысит конверсию
+          посетителей в покупателей
+        </span>
+      </div>
+      <div className={styles.Block1_Img}>
+        <img src={Img} alt="eye" />
+      </div>
+    </div>
+  );
+};
+
+export default Block1;
--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-
+import Block1 from '../../components/Block1/Block1';
+import styles from './MainPage.module.scss'
+import Block5 from '../../components/Block5/Block5';
 const MainPage = () => {
     return (
-        <div>
+        <div className={styles.MainPage}>
             <Header />
+            <Block1 />
+            <Block5 />
         </div>
     );
 };
